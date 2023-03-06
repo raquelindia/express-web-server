45f5f0655ef84a2b4bc79893bf5039d4783a1388
raquelindia <raquelicruz94@gmail.com>
2023-03-06 11:27:42 -0500

    added server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const app = express();
 
+const port = 3000;
+
+app.use(express.static('public'));
+app.listen(port, () => {
+    console.log(`server listening at http://localhost: ${port}`);
+})
